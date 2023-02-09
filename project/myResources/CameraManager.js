95f24bf5a61878e788b6850e1c08bc78c0850ff9
LucaLand <luca.landolfi3@studio.unibo.it>
2023-02-09 15:17:35 +0100

    LucaLan FCG - Added working camera FollowingObj and LookAtObj features

--- project/myResources/CameraManager.js
+++ project/myResources/CameraManager.js
@@ -7,7 +7,7 @@ let CameraManager = function (gl){
 
     this.fieldOfViewRadians = degToRad(60);
     this.cameraPosition = [5,0,5];
-    this.target = [0, 0, 0];
+    this.targetPosition = [0, 0, 0];
     this.up = [0, 1, 0];
     this.zmin = 0.1;
     this.zfar = 200;
@@ -17,21 +17,59 @@ let CameraManager = function (gl){
     this.phi = degToRad(90);     //Angolo su z
 
     let DLimitMax = 10, DLimitMin = 1;
+    let lookingAtTargetObj = true;
+    let followingObj = true;
+    let targetObj = null;
+    let cameraPositionCenter = [0,0,0];
 
+    this.setTargetObj = function (meshObj){
+        targetObj = meshObj;
+        this.setFollowTargetObj(true);
+    }
 
- this.computeMatrix = function (){
-     // Compute the projection matrix
-     let aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-     this.projectionMatrix = m4.perspective(this.fieldOfViewRadians, aspect, this.zmin, this.zfar);
+    this.setLookAtTargetObj = function (bool){
+        lookingAtTargetObj = bool;
+    }
 
-     // Compute the camera's matrix using look at.
-     this.cameraMatrix = m4.lookAt(this.cameraPosition, this.target, this.up);
+    this.setFollowTargetObj = function (bool){
+        followingObj = bool;
+        lookingAtTargetObj = bool;
+    }
 
-     // Make a view matrix from the camera matrix.
-     this.viewMatrix = m4.inverse(this.cameraMatrix);
- }
+    this.computeCameraMatrixWithPolarCords = function (){
+        let D = this.distance;
+        let c = cameraPositionCenter;
+        this.cameraPosition =[(D * Math.sin(this.theta) * Math.sin(this.phi)) + c[0],
+            (D * Math.cos(this.phi)) + c[1],
+            (D * Math.cos(this.theta) * Math.sin(this.phi)) + c[2]];
+        //To go upside down (don't make camera twist in the pole)
+        this.up = [0, Math.sin(this.phi), 0];
+        //this.computeMatrix();
+    }
 
- this.computeMatrix();
+     this.computeMatrix = function (){
+
+         this.computeCameraMatrixWithPolarCords();
+         // Compute the projection matrix
+         let aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+         this.projectionMatrix = m4.perspective(this.fieldOfViewRadians, aspect, this.zmin, this.zfar);
+
+         //Setting target on obj
+         if(lookingAtTargetObj && targetObj !== null){
+             this.targetPosition = targetObj.getPosition();
+             if(followingObj)
+                 cameraPositionCenter = targetObj.getPosition();
+         }
+
+         // Compute the camera's matrix using look at.
+         this.cameraMatrix = m4.lookAt(this.cameraPosition, this.targetPosition, this.up);
+
+         // Make a view matrix from the camera matrix.
+         this.viewMatrix = m4.inverse(this.cameraMatrix);
+     }
+
+     //Computing Matrix first time this is created
+    this.computeMatrix();
 
      this.setCameraAttrDefault1 = function (){
         const camera = this;
@@ -48,14 +86,12 @@ let CameraManager = function (gl){
 
         this.theta = degToRad(0);
         this.phi = degToRad(90);
-        camera.computeCameraMatrixWithPolarCords();
         camera.computeMatrix(); //Camera Compute Matrix
     }
 
     this.rotateCamera = function (dTheta, dPhi){
         this.theta += dTheta;
         this.phi += dPhi;
-        this.computeCameraMatrixWithPolarCords();
     }
 
     this.translateCameraDistance = function (deltaDistance) {
@@ -63,39 +99,8 @@ let CameraManager = function (gl){
         if((d + deltaDistance) < DLimitMax && (d + deltaDistance) > DLimitMin)
             this.distance += deltaDistance;
         console.log("Camera Distance: " + this.distance)
-        this.computeCameraMatrixWithPolarCords();
     }
 
-    this.computeCameraMatrixWithPolarCords = function (){
-        let D = this.distance;
-         this.cameraPosition =[D * Math.sin(this.theta) * Math.sin(this.phi),
-            D * Math.cos(this.phi),
-            D * Math.cos(this.theta) * Math.sin(this.phi)];
-        //To go upside down (don't make camera twist in the pole)
-        this.up = [0, Math.sin(this.phi), 0];
-        this.computeMatrix();
-    }
-
-    // this.setCameraPositionWithAngles = function (theta, phi){
-    //      let D = this.distance;
-    //      this.cameraPosition =[D*Math.sin(phi)*Math.cos(theta),
-    //          D*Math.sin(phi)*Math.sin(theta),
-    //          D*Math.cos(phi)];
-    //      updateCameraAngles(theta, phi);
-    //      this.computeMatrix();
-    // }
-    //
-    // this.getCameraAngles = function (){
-    //      return {
-    //          theta: this.theta,
-    //          phi: this.phi
-    //      }
-    // }
-    //
-    // function updateCameraAngles(t, p){
-    //      this.theta = t;
-    //      this.phi = p;
-    // }
 
  //TODO. Add setter and getter for camera data (and autocompute matrix on this changes -  add this.matrixAutoUpdate = true)
 
