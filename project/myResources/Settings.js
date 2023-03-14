9b296a198a23c0a04d71de90f656575e8c034b7c
LucaLand <luca.landolfi3@studio.unibo.it>
2023-03-14 10:45:15 +0100

    LucaLan FCG - Added Settings.js

---
+++ project/myResources/Settings.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const Settings = {
+    time: 1, //Light Direction and time
+    sun: true,
+    lamps: false,
+    shadows: true,
+    reflections: true,
+    transparency: true,
+    fov: degToRad(60),
+    filterColors: [0.2, 0.2, 0.2] //Light color
+}
