abb3874af698f97dc18d6f8161f04aaf663063d5
LucaLand <luca.landolfi3@studio.unibo.it>
2023-03-16 12:32:26 +0100

    LucaLan FCG - Changed Light parameters (some shadows appeared)

--- project/myResources/Light.js
+++ project/myResources/Light.js
@@ -5,18 +5,21 @@ const Light = function () {
     let ambientLight = [0.2, 0.2, 0.2];
     let colorLight = [1.0, 1.0, 1.0];
     let colorLightVec4 = [1.0, 1.0, 1.0, 1.0];
-    let lightDirection = m4.normalize([-1, 3, 5]);
-    let lightPosition = [0, -1000, 0];
+    let lightDirection = [0,0,1]; //Target
+    // Direction -> //m4.normalize([-1, 3, 5]);
+    let lightPosition = [0, 0, 0];
     let perspective = false;
     let lightFov = degToRad(60);
     let lightProjectionMatrix;
     let lightWorldMatrix;
 
-    this.innerLimit = 1.0;
-    this.outerLimit = 100.0;
+    this.innerLimit = -1.0;
+    this.outerLimit = 1;
+    this.near = 0.5;
+    this.far = 1000;
 
-    this.projWidth = 100;
-    this.projHeight = 100;
+    this.projWidth = 10;
+    this.projHeight = 10;
 
     this.setLightDirection = function (x, y, z){
         lightDirection = m4.normalize([x, y, z]);
@@ -70,8 +73,8 @@ const Light = function () {
         // first draw from the POV of the light
         lightWorldMatrix = m4.lookAt(
             this.getLightPosition(),          // position
-            this.getLightDirection(), // target
-            [0, 1, 0],                                              // up
+            this.getLightDirection(),         // target
+            [0, 0, -1],                       // up
         );
         return lightWorldMatrix;
     }
@@ -81,15 +84,15 @@ const Light = function () {
             ? m4.perspective(
                 degToRad(this.getLightFov()),
                 this.projWidth / this.projHeight,
-                0.5,  // near
-                10)   // far
+                this.near,  // near
+                this.far)   // far
             : m4.orthographic(
                 -this.projWidth / 2,   // left
                 this.projWidth / 2,   // right
                 -this.projHeight / 2,  // bottom
                 this.projHeight / 2,  // top
-                0.5,                      // near
-                10);                      // far
+                this.near,                      // near
+                this.far);                      // far
         return lightProjectionMatrix;
     }
 }
