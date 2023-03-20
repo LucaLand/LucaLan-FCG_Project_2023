a40cb1d073e33d423a97d2b4601a7b11e9bdf098
LucaLand <luca.landolfi3@studio.unibo.it>
2023-03-20 12:55:42 +0100

    LucaLan FCG - Fixed light target (removed direction) and light target set function

--- project/myResources/Light.js
+++ project/myResources/Light.js
@@ -5,10 +5,10 @@ const Light = function () {
     let ambientLight = [0.2, 0.2, 0.2];
     let colorLight = [1.0, 1.0, 1.0];
     let colorLightVec4 = [1.0, 1.0, 1.0, 1.0];
-    let lightPosition = [0, 30, 0];
-    let lightDirection = [50, 0, 0]; //Target
+    let lightPosition = [0, 10, 0];
+    let lightTarget = [10, 0, 0];
     // Direction -> //m4.normalize([-1, 3, 5]);
-    let lightFov = degToRad(160);
+    let lightFov = degToRad(100);
 
     let perspective = true;
     this.innerLimit = Math.cos(degToRad(lightFov / 2 - 10));
@@ -19,12 +19,12 @@ const Light = function () {
     this.projWidth = 100;
     this.projHeight = 100;
 
-    this.setLightDirection = function (x, y, z){
-        lightDirection = m4.normalize([x, y, z]);
+    this.setLightTarget = function (x, y, z){
+        lightTarget = [x,y,z];      //m4.normalize([x, y, z]); Direction
     }
 
-    this.getLightDirection = function (){
-        return lightDirection;
+    this.getLightTarget = function (){
+        return lightTarget;
     }
 
     this.setAmbientLight = function (R = 0.2, G= 0.2, B = 0.2){
@@ -69,12 +69,11 @@ const Light = function () {
 
     this.computeLightWorldMatrix = function (){
         // first draw from the POV of the light
-        let lightWorldMatrix = m4.lookAt(
+        return m4.lookAt(
             lightPosition,          // position
-            lightDirection,         // target
-            [0, 1, 0]                       // up
+            lightTarget,         // target
+            [0, 0, 1],              // up
         );
-        return lightWorldMatrix;
     }
 
     this.computeLightProjectionMatrix = function (){
